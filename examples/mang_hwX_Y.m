9c687ed831312c2b90f42265e151bd378c6d109c
Andreas Mang <andreas@ices.utexas.edu>
2019-01-24 16:12:07 -0600

    example for homework submission added

---
+++ examples/mang_hwX_Y.m
@@ -0,0 +1,18 @@
+clear, close all;
+clc;
+
+% for loop from k = 2 to 5 with increment 1 to create
+% a vector of size k x 1
+for k = 2:5
+	% for every k, generate a vector
+	% of size k x 1 with random entries
+	
+    x = rand(k,1);
+
+	% display size of vector
+	fprintf('size of vector: %i x %i\n',size(x));
+
+	% display vector					
+	x
+end
+
