2ce5047674a5e26fa7ad8318adaacd0dd371595f
Andreas Mang <andreas@math.uh.edu>
2019-06-17 22:06:17 -0500

    first matlab homework template added

---
+++ homework/hw01a.m
@@ -0,0 +1,14 @@
+clear, close all;
+
+% create matrix of size 7x10
+A = % ADD YOUR CODE HERE
+
+% create random vector of size 10x1
+x = % ADD YOUR CODE HERE
+
+% compute matrix vector product
+b = % ADD YOUR CODE HERE
+
+% display result
+disp('b = Ax:');
+disp(b);
