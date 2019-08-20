9d4b4fe419c82592bd63b174b77c33d07db0c95f
Andreas Mang <andreas@math.uh.edu>
2019-08-20 12:58:42 -0500

    new homework template added

---
+++ homework/hw02a.m
@@ -0,0 +1,16 @@
+clear, close all; clc;
+
+% create matrix of size 10 x 10 using the 'magic' cmd
+A = % ADD YOUR CODE HERE
+
+% create 10 x 1 vector of all ones
+u = % ADD YOUR CODE HERE
+
+% compute matrix vector product
+y = % ADD YOUR CODE HERE
+
+% display result
+fprintf('y = \n');
+disp(y);
+
+% DESCRIBE YOUR OBSERVATIONS BELOW
