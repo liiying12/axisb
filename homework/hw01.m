620543e9a06e1d381af02b03e20b227cbbe7f3f8
liiying12 <60397476+liiying12@users.noreply.github.com>
2020-01-28 09:51:37 -0600

    Update hw01.m

--- homework/hw01.m
+++ homework/hw01.m
@@ -1,13 +1,13 @@
 clear, close all; clc;
 
 % create a random matrix of size 7x10
-A = % ADD YOUR CODE HERE
+A = randi([0 1],7,10) ans% ADD YOUR CODE HERE
 
 % create random vector of size 10x1
-x = % ADD YOUR CODE HERE
+x =randi([0 1],10,1) ans % ADD YOUR CODE HERE
 
 % compute matrix vector product
-b = % ADD YOUR CODE HERE
+b = randi([7,1) ans% ADD YOUR CODE HERE
 
 % display result
 disp('b = Ax:');
